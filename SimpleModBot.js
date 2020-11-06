00f8d6947e81613496e6ed56244e43cc34965f8e
xWass <73602180+xWass@users.noreply.github.com>
2020-11-06 08:40:41 -0600

    Updated bot with Mute and Unmute
    Untested Mute and Unmute command - works on the name of the role being "Muted"

--- SimpleModBot.js
+++ SimpleModBot.js
@@ -1,33 +1,70 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
+const Discord = require("discord.js");
+const config = require("./config.json");
 const client = new Discord.Client({ disableEveryone: true });
+const member = message.mentions.members.first();
+const role = message.guild.roles.cache.find(r => r.name === "Muted");
 
-client.on('ready', async function () {
-    console.log(client.user.username + ' is ready!');
-    client.user.setActivity(' ', { type: 'LISTENING' });
+client.on("ready", async function () {
+    console.log(client.user.username + " is ready!");
+    client.user.setActivity("to my own sorrow.", { type: "LISTENING" });
 });
-client.on('message', async (message) => {
+client.on("message", async (message) => {
     if (
         !message.content.startsWith(config.prefix) ||
         message.author.bot ||
-        message.channel.type === 'dm'
+        message.channel.type === "dm"
     )
         return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
     
     if (command === `welcome`) {
+      message.channel.send("Welcome to the server! :wave:");
       message.delete();
-      message.channel.send('Welcome to the server! :wave:');
-      return
+      return;
     }
-    if (command === `help`) {
+    if (command === `commands`) {
+        message.channel.send("Use !kick to kick a user. \nUse !ban to ban a user, and !unban to unban a user. \nUse !mute to mute a user, and !unmute to unmute a user.");
         message.delete();
-        message.channel.send(
-            "Use !kick to kick a user. \nUse !ban to ban a user, and !unban to unban a user. \nUse !pog because why not. \mUse !ping to check your ping. ");
           return;
     }
+  
+    if (command ===`mute`) {
+      message.guild.roles.cache.find(r => r.name === "Muted");
+      message.delete();
+      if(!message.member.hasPermission("MANAGE_ROLES")){
+        message.channel.send("You do not have permission to use this command");
+        return;
+      }
+      if(!message.guild.me.hasPermission("MANAGE_ROLES")) {
+        message.channel.send('I do not have permission to use this command');
+        return;
+      }
+      if(!user){
+        message.channel.send("You didn't mention anyone!");
+        return;
+      }
+      member.roles.add(role);
+    }
+    if (command ===`unmute`) {
+      message.guild.roles.cache.find(r => r.name === "Muted");
+      message.delete();
+      if(!message.member.hasPermission("MANAGE_ROLES")){
+        message.channel.send("You do not have permission to use this command");
+        return;
+      }
+      if(!message.guild.me.hasPermission("MANAGE_ROLES")) {
+        message.channel.send('I do not have permission to use this command');
+        return;
+      }
+      if(!user){
+        message.channel.send("You didn't mention anyone!");
+        return;
+      }
+      member.roles.remove(role);
+    }
     if (command === `kick`) {
+      message.delete();
       if(!message.member.hasPermission("KICK_MEMBERS")) {
         return message.channel.send(`**${message.author.username}**, You must be a moderator to use this command`)
       }
@@ -43,17 +80,18 @@ client.on('message', async (message) => {
             .then((member) => {
                 // Successmessage
                 message.channel.send(
-                    ':wave: ' +
+                    ":wave: " +
                         member.displayName +
-                        ' has been successfully kicked :point_right: '
+                        " has been successfully kicked :point_right: "
                 );
             })
             .catch(() => {
                 // Failmessage
-                message.channel.send('Access Denied');
+                message.channel.send("Access Denied");
             });
     }
     if (command === `ban`) {
+      message.delete();
       if(!message.member.hasPermission("BAN_MEMBERS")) {
         return message.channel.send(`**${message.author.username}**, You must be a moderator to use this command`)
       }
@@ -62,42 +100,42 @@ client.on('message', async (message) => {
         return message.channel.send(`**${message.author.username}**, I can not unban this user because I do not have the permission to do so.`)
       }
         // Easy way to get member object though mentions.
-        const member = message.mentions.members.first();
         // ban
         member
             .ban()
             .then((member) => {
                 // Successmessage
                 message.channel.send(
-                    ':wave: ' +
-                        member.displayName +
-                        ' has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: '
+                    `:wave: ${member.displayName} has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: `
                 );
             })
             .catch(() => {
                 // Failmessage
-                message.channel.send('Access Denied');  
+                message.channel.send("Access Denied");  
             });
     }
     if(command === "unban"){
-      if(!message.member.hasPermission("BAN_MEMBERS")) {
-        return message.channel.send(`**${message.author.username}**, You must be a moderator to use this command`)
-      }
+      message.delete();
+        if(!message.member.hasPermission("BAN_MEMBERS")) {
+          return message.channel.send(`**${message.author.username}**, You must be a moderator to use this command`)
+        }
 
-      if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
-        return message.channel.send(`**${message.author.username}**, I can not unban this user because I do not have the permission to do so.`)
-      }
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
+          return message.channel.send(`**${message.author.username}**, I can not unban this user because I do not have the permission to do so.`)
+        }
 
-      let userID = args[0]
-        message.guild.fetchBans().then(bans=> {
-        if(bans.size == null) return
-        let bUser = bans.find(b => b.user.id == userID)
-        if(!bUser) return
-        message.guild.members.unban(bUser.user)
-        return message.channel.send(
-          ':wave: User has been successfully unbanned'
-        )
-  })
-}
+        let userID = args[0]
+          message.guild.fetchBans().then((bans) => {
+              if (bans.size == null)
+                return;
+              let bUser = bans.find(b => b.user.id == userID);
+              if (!bUser)
+                return;
+              message.guild.members.unban(bUser.user);
+              return message.channel.send(
+                ":wave: User has been successfully unbanned"
+              );
+            });
+          }
 });
 client.login(config.token)
